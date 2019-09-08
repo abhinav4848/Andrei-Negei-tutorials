ed16d9e3c7eeed30bc0df285e3ffa6060305f9e5
Abhinav <abhinav4848@gmail.com>
2019-09-08 13:53:15 +0530

    2nd video on robofriends

--- react/robofriends/src/Card.js
+++ react/robofriends/src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, username }) => {
     // const { name, email, id } = props; where props would be the argument received
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${id}? size=200x200`} alt='robot via templete literal' />
+            <img src={`https://robohash.org/${id}? size=200x200`} alt='robot via template literal' />
             <div>
                 <h2>{name}</h2>
-                <p>{email}</p>
+                <p>{username}: {email}</p>
             </div>
         </div>
     );
