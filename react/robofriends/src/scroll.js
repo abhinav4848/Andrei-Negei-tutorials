9cd08aab84d88a33ca3343d78e2a9854143664d6
Abhinav <abhinav4848@gmail.com>
2019-09-08 22:17:04 +0530

    comment following vid 5

--- react/robofriends/src/scroll.js
+++ react/robofriends/src/scroll.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '1px solid black', height: '700px' }}>
+        // in jsx, add style by {{}}. Outer brace is js expression, inner is object
+        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '700px' }}>
             {props.children}
         </div>
     )
