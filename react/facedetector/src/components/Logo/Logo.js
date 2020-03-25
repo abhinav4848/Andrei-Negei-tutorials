c47b62d9cc8a704925662884a19dbafd2660716e
Abhinav <abhinav4848@gmail.com>
2020-03-25 22:04:41 +0530

    Update Logo.js

--- react/facedetector/src/components/Logo/Logo.js
+++ react/facedetector/src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
+        <div className="ma4 mt0" style={{ float: 'left' }}>
             <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 250, width: 250 }} >
                 <div className="Tilt-inner pa5">
                     <img style={{ paddingTop: '5px' }} alt="Logo" src={brain} />
